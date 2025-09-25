3c191c70228577f13358a30afb5a0f494c4701ab
Juan Sebastian Ruiz Aguilar <juansebas064@gmail.com>
2025-09-25 15:05:40 -0500

    Project initialization

---
+++ src/test/java/com/app/lend_it_backend/LendItBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.lend_it_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LendItBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
